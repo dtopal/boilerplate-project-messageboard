bcf4f64fdef985aa3839857d8e881ec527e8d0b4
dtopal <11358089+dtopal@users.noreply.github.com>
2020-02-08 18:40:21 +0900

    Add packages and secruity middleware

--- server.js
+++ server.js
@@ -4,12 +4,18 @@ var express     = require('express');
 var bodyParser  = require('body-parser');
 var expect      = require('chai').expect;
 var cors        = require('cors');
+var helmet      = require('helmet');
 
 var apiRoutes         = require('./routes/api.js');
 var fccTestingRoutes  = require('./routes/fcctesting.js');
 var runner            = require('./test-runner');
 
 var app = express();
+
+app.use(helmet.dnsPrefetchControl());
+app.use(helmet.frameguard({ action: 'sameorigin' }));
+app.use(helmet.referrerPolicy({ action: 'same-origin' }));
+
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
@@ -37,18 +43,11 @@ app.route('/')
 //For FCC testing purposes
 fccTestingRoutes(app);
 
-//Routing for API 
+//Routing for API
 apiRoutes(app);
 
 //Sample Front-end
 
-    
-//404 Not Found Middleware
-app.use(function(req, res, next) {
-  res.status(404)
-    .type('text')
-    .send('Not Found');
-});
 
 //Start our server and tests!
 app.listen(process.env.PORT || 3000, function () {
